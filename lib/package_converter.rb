6744478f006952177af7a36765472c48cdae9b48
Rainer Blessing <rainer.blessing@googlemail.com>
2013-06-23 17:21:43 +0200

    added check to skip package conversion

--- lib/package_converter.rb
+++ lib/package_converter.rb
@@ -10,11 +10,17 @@ class PackageConverter
     full_dest_directory=File.join(@root,dest_dir)
     rpms = File.join(full_src_directory,"*.rpm")
     debs = File.join(full_dest_directory,"*.deb")
-    delete debs
-    system("cd #{full_dest_directory} && fakeroot alien --scripts #{rpms}")
+    unless compare_size(rpms,debs) 
+      delete debs
+      system("cd #{full_dest_directory} && fakeroot alien --scripts #{rpms}")
+    end
   end
 
   private
+
+  def compare_size dir1,dir2
+    Dir.glob(dir1).size == Dir.glob(dir2).size
+  end
 
   def delete(filepattern)
     Dir[filepattern].each do |file|
